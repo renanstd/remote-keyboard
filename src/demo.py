60175341b25d1139d41696200fdcf5756833faf0
Renan Santana <renan_ota@hotmail.com>
2021-02-10 23:39:55 -0300

    Layout basico

---
+++ src/demo.py
@@ -0,0 +1,6 @@
+from dearpygui.demo import show_demo
+from dearpygui.core import start_dearpygui
+
+
+show_demo()
+start_dearpygui()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
-from dearpygui.core import *
-from dearpygui.simple import *
+from dearpygui import core, simple
 
 
-with window("Remote Keyboard"):
-    add_text("Teste")
+core.set_main_window_size(300, 400)
+# Callbacks
+def activate(sender, data):
+    # Disable inputs
+    core.configure_item("IP", enabled=False)
+    core.configure_item("Port", enabled=False)
+    # core.configure_item("Content", enabled=True)
 
-start_dearpygui()
+def send_key(sender, data):
+    print("Type")
+
+with simple.window("Remote Keyboard"):
+    # Title
+    core.add_text("Remote Keyboard 1.0")
+    core.add_separator()
+    # Inputs
+    core.add_input_text("IP", label="Device IP")
+    core.add_input_text("Port", label="Device port")
+    core.add_separator()
+    # Button
+    core.add_selectable("Activate", callback=activate)
+    # core.add_input_text(
+    #     "Content",
+    #     multiline=True,
+    #     enabled=False,
+    #     label="",
+    #     width=260,
+    #     callback=send_key
+    # )
+
+
+core.start_dearpygui(primary_window="Remote Keyboard")
